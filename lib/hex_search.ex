6272404a058d98eac575511003976dc92e5fbe55
Khaja Minhajuddin <minhajuddink@gmail.com>
2018-08-11 18:12:48 +0530

    init

---
+++ lib/hex_search.ex
@@ -0,0 +1,18 @@
+defmodule HexSearch do
+  @moduledoc """
+  Documentation for HexSearch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HexSearch.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hex_search_test.exs
@@ -0,0 +1,8 @@
+defmodule HexSearchTest do
+  use ExUnit.Case
+  doctest HexSearch
+
+  test "greets the world" do
+    assert HexSearch.hello() == :world
+  end
+end
