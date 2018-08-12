c5aa27f104bbbfb660209e2c6df2332f77e6d871
Khaja Minhajuddin <minhajuddink@gmail.com>
2018-08-12 10:19:31 +0530

    save

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
 # HexSearch
 
-**TODO: Add description**
+Hex Search is a CLI tool which allows you to search hex packages from the terminal
 
-## Installation
+## FIXME Installation
 
+# FIXME
 If [available in Hex](https://hex.pm/docs/publish), the package can be installed
 by adding `hex_search` to your list of dependencies in `mix.exs`:
 
-```elixir
-def deps do
-  [
-    {:hex_search, "~> 0.1.0"}
-  ]
-end
-```
-
+# FIXME
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/hex_search](https://hexdocs.pm/hex_search).
--- lib/hex_search.ex
+++ lib/hex_search.ex
@@ -1,18 +1,63 @@
 defmodule HexSearch do
-  @moduledoc """
-  Documentation for HexSearch.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def main([search_term]) do
+    :inets.start()
+    :ssl.start()
+    config = :hex_core.default_config()
+    options = [sort: :downloads]
 
-  ## Examples
+    ansi_opts = IEx.Config.ansi_docs()
 
-      iex> HexSearch.hello
-      :world
+    case :hex_api_package.search(config, search_term, options) do
+      {:ok, {200, _, packages}} ->
+        Enum.each(packages, fn p ->
+          p |> render |> print(ansi_opts)
+        end)
 
-  """
-  def hello do
-    :world
+      err ->
+        Logger.error("ERROR: #{inspect(err)}")
+    end
+  end
+
+  def main(_) do
+    IO.puts("""
+    Invalid args
+
+    Usage
+        hex_search name-of-hex-package
+    """)
+  end
+
+  def render(package) do
+    %{
+      heading:
+        "name: #{package["name"]} downloads: #{package["downloads"]["all"]} licenses: #{
+          Enum.join(package["meta"]["licenses"], ", ")
+        }",
+      body: """
+      dep: {:#{package["name"]}, "~> #{hd(package["releases"])["version"]}"}
+
+      links: #{package["meta"]["links"] |> Enum.map(fn {text, href} -> "[#{text}](#{href})" end) |> Enum.join(", ")}
+
+      maintaners: #{Enum.join(package["meta"]["maintainers"], ", ")}
+
+      last update: #{package["updated_at"]}
+
+      #{package["meta"]["description"]}
+
+      """
+
+    }
+  end
+
+  def print(package, nil) do
+    IO.puts(["# ", package.heading, ?\n])
+    IO.puts(package.body)
+  end
+
+  def print(package, opts) do
+    IO.ANSI.Docs.print_heading(package.heading, opts)
+    IO.ANSI.Docs.print(package.body, opts)
   end
 end
