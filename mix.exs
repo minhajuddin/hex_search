31a4bfdcb1eb52dd33a1583160fd0a6556ab4534
Khaja Minhajuddin <minhajuddink@gmail.com>
2018-08-12 18:52:04 +0530

    setup hex info

--- mix.exs
+++ mix.exs
@@ -4,6 +4,12 @@ defmodule HexSearch.MixProject do
   def project do
     [
       app: :hex_search,
+      name: "HexSearch",
+      description: description(),
+      package: package(),
+      docs: [
+        extras: ~W(README.md)
+      ],
       version: "0.1.0",
       elixir: "~> 1.6",
       escript: [main_module: HexSearch],
@@ -22,7 +28,27 @@ defmodule HexSearch.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:hex_core, "> 0.0.0"}
+    ]
+  end
+
+  defp description do
+    """
+    A simple utility that allows you to search through the hex packages.
+    """
+  end
+
+  defp package do
+    [
+      description: description(),
+      files: ~w(lib config mix.exs README.md LICENSE),
+      maintainers: ["Khaja Minhajuddin"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "http://github.com/minhajuddin/hex_search",
+        "Docs" => "http://hexdocs.pm/hex_search"
+      }
     ]
   end
 end
